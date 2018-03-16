da509e01a1d3e60ff2ccfa1d0866dd9e8addcfb3
goyalarchit29 <archit.goyal29@gmail.com>
2018-03-17 04:48:00 +0530

    Initial Layout

---
+++ app/src/main/java/com/example/architgoyal/hint18/alphaCanvasActivity.java
@@ -0,0 +1,13 @@
+package com.example.architgoyal.hint18;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class alphaCanvasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alpha_canvas);
+    }
+}
---
+++ app/src/main/java/com/example/architgoyal/hint18/wordsCanvasActivity.java
@@ -0,0 +1,13 @@
+package com.example.architgoyal.hint18;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class wordsCanvasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_words_canvas);
+    }
+}
