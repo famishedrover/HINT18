764d77c0525becf9efb87bbe2803e193590d98e5
pradyumn014 <pradyumna014@gmail.com>
2018-03-17 10:52:40 +0530

    funcs2

--- Backend/webhook/views.py
+++ Backend/webhook/views.py
@@ -13,7 +13,7 @@ import base64
 import io
 
 from models import *
-import letter_funcs import *
+from letter_funcs import *
 transactions_map=[(A_transactions,'A'),(B_transactions,'B')]
 letter_funcs_map=[f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13,f14,f15,f16,f17,f18,f19,f20,f21,f22,f23,f24,f25,f26]
 ## curl -H "Content-Type: application/json" -X POST -d '{"id":1,"number":5}' http://localhost:8000/webhook/letter/
