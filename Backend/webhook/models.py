211fcc15a1e42735a67ffe799be8a7dc99776948
pradyumn014 <pradyumna014@gmail.com>
2018-03-17 09:55:09 +0530

    Models Finished

--- Backend/webhook/models.py
+++ Backend/webhook/models.py
@@ -6,15 +6,145 @@ from django.db import models
 
 class child_model(models.Model):
 	id=models.AutoField(default=0,primary_key=True)
-	name=models.CharField(maxlength=50)
+	name=models.CharField(max_length=50)
 	age=models.IntegerField()
 	total_evaluations=models.IntegerField(default=0)
 	score=models.IntegerField(default=100)
 
 class parent_model(models.Model):
-	id=models.AutoField(deault=0,primary_key=True)
-	name=models.CharField(maxlength=50)
+	id=models.AutoField(default=0,primary_key=True)
+	name=models.CharField(max_length=50)
 	child_id=models.IntegerField()
+	A=models.IntegerField()
+	B=models.IntegerField()
+	C=models.IntegerField()
+	D=models.IntegerField()
+	E=models.IntegerField()
+	F=models.IntegerField()
+	G=models.IntegerField()
+	H=models.IntegerField()
+	I=models.IntegerField()
+	J=models.IntegerField()
+	K=models.IntegerField()
+	L=models.IntegerField()
+	M=models.IntegerField()
+	N=models.IntegerField()
+	O=models.IntegerField()
+	P=models.IntegerField()
+	Q=models.IntegerField()
+	R=models.IntegerField()
+	S=models.IntegerField()
+	T=models.IntegerField()
+	U=models.IntegerField()
+	V=models.IntegerField()
+	W=models.IntegerField()
+	X=models.IntegerField()
+	Y=models.IntegerField()
+	Z=models.IntegerField()
+
+# class A_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
+
+# class B_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
+
+# class C_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
+
+# class D_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
+
+# class E_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
+
+# class F_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
+
+# class G_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
+
+# class H_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
+
+# class I_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
+
+# class J_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
+
+# class K_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
+
+# class L_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
+
+# class M_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
+
+# class N_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
+
+# class O_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
+
+# class P_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
+
+# class Q_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
+
+# class R_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
+
+# class S_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
+
+# class T_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
+
+# class U_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
+
+# class V_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
+
+# class W_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
+
+# class X_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
+
+# class Y_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
+
+# class Z_values(models.Model):
+# 	child_id=models.IntegerField()
+# 	score=models.IntegerField()
 
 #Keyed by evaluation no and child_id
 class A_transactions(models.Model):
@@ -23,7 +153,7 @@ class A_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 	
 
@@ -33,7 +163,7 @@ class B_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 
 class C_transactions(models.Model):
@@ -42,7 +172,7 @@ class C_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 
 class D_transactions(models.Model):
@@ -51,7 +181,7 @@ class D_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 
 class E_transactions(models.Model):
@@ -60,7 +190,7 @@ class E_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 
 class F_transactions(models.Model):
@@ -69,7 +199,7 @@ class F_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 
 class G_transactions(models.Model):
@@ -78,7 +208,7 @@ class G_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 
 class H_transactions(models.Model):
@@ -87,7 +217,7 @@ class H_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 
 class I_transactions(models.Model):
@@ -96,7 +226,7 @@ class I_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 
 class J_transactions(models.Model):
@@ -105,7 +235,7 @@ class J_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 
 class K_transactions(models.Model):
@@ -114,7 +244,7 @@ class K_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 
 class L_transactions(models.Model):
@@ -123,7 +253,7 @@ class L_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 
 class M_transactions(models.Model):
@@ -132,7 +262,7 @@ class M_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 
 class N_transactions(models.Model):
@@ -141,7 +271,7 @@ class N_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 
 class O_transactions(models.Model):
@@ -150,7 +280,7 @@ class O_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 
 class P_transactions(models.Model):
@@ -159,7 +289,7 @@ class P_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 
 class Q_transactions(models.Model):
@@ -168,7 +298,7 @@ class Q_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 
 class R_transactions(models.Model):
@@ -177,7 +307,7 @@ class R_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 
 class S_transactions(models.Model):
@@ -186,7 +316,7 @@ class S_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 
 class T_transactions(models.Model):
@@ -195,7 +325,7 @@ class T_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 
 class U_transactions(models.Model):
@@ -204,7 +334,7 @@ class U_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 
 class V_transactions(models.Model):
@@ -213,7 +343,7 @@ class V_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 
 class W_transactions(models.Model):
@@ -222,7 +352,7 @@ class W_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 
 class X_transactions(models.Model):
@@ -231,7 +361,7 @@ class X_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 
 class Y_transactions(models.Model):
@@ -240,7 +370,7 @@ class Y_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 
 class Z_transactions(models.Model):
@@ -249,7 +379,6 @@ class Z_transactions(models.Model):
 	score=models.IntegerField(default=100)
 	total_time=models.FloatField()
 	no_of_strokes=models.IntegerField()
-	total_length()=models.FLoatField(default=0)
+	total_length=models.FloatField(default=0)
 	time_delay=models.FloatField()
 	
-
